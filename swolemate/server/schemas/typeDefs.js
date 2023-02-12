33cff878339f4d71e3c22bb60c7c0fd99fb9a9d1
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2023-02-11 22:14:39 -0600

    typeDefs.js updates

---
+++ swolemate/server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    profile(username: String): [Profile]
+  }
+
+type Profile {
+    _id: ID
+    name: String
+    skills: [String]!
+  }
+
+  type Mutation {
+    addProfile(name: String!): Profile
+    removeProfile(profileId: ID!): Profile
+    
+  }
+`;
+
+module.exports = typeDefs;
