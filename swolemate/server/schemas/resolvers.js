58c2f74b73b463be9b124cb716ba0e77b6cd4ab2
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2023-02-11 21:40:00 -0600

    updae to resolver.js

--- swolemate/server/schemas/resolvers.js
+++ swolemate/server/schemas/resolvers.js
@@ -1,9 +1,18 @@
-const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Profile } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-  Query: {
-   
-  }
+    Query: {
+        users: async () => {
+          return User.find().populate('profile');
+        },
+        user: async (parent, { username }) => {
+          return User.findOne({ username }).populate('profile');
+        
+        },
+      },
+    
 };
 
 module.exports = resolvers;
