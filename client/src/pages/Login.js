81de06f6fc56ebaa4c12ecc37f7b8c042dfb717b
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2023-02-23 00:59:09 -0600

    redirect login to profile page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -53,7 +53,7 @@ const Login = (props) => {
     // });
   };
   if (Auth.loggedIn()) {
-    return <Navigate to="/"  replace={true} />;
+    return <Navigate to="/profile"  replace={true} />;
   }
   
 
